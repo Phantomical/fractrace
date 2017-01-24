1b2d24a9866e6665be1897977301348c7744a8d4
Phantomical <phantom@lynches.ca>
2017-01-23 19:17:19 -0500

    Added substraction operator

---
+++ src/fractrace/shapes/Substraction.java
@@ -0,0 +1,23 @@
+package fractrace.shapes;
+
+import fractrace.Traceable;
+import fractrace.Vector;
+
+public class Substraction implements Traceable {
+	private Traceable object1;
+	private Traceable object2;
+	private Vector colour;
+	
+	@Override
+	public double distanceFromPoint(Vector a) {
+		return Math.max(
+				-object1.distanceFromPoint(a),
+				object2.distanceFromPoint(a));
+	}
+
+	@Override
+	public Vector colourAtPoint(Vector a) {
+		return colour;
+	}
+
+}
